5902c91c993590e2350e1d111768c480dd695cb0
coZmoKramer <jtoland3.14@gmail.com>
2017-03-29 22:42:42 -0500

    Camera Follow, Save State
    Jump animation, Grow/Shrink animation, fire animation
    Camera follow, save states and kill boxes
    Finished with tutorial 6

--- MarioBros2/Assets/2D Mario Assets/Scripts/playerProperties.js
+++ MarioBros2/Assets/2D Mario Assets/Scripts/playerProperties.js
@@ -1,11 +1,117 @@
-#pragma strict
+//Player Properties Component
+//Description: Set and Stores pickups and state of player
 
-function Start ()
-{
+	@script AddComponentMenu("WalkerBoys/Actor/Player Properties Script")
+	//enumeration
+	enum PlayerState
+	{
+		MarioDead 	= 0,
+		MarioSmall 	= 1,
+		MarioLarge 	= 2,
+		MarioFire 	= 3
+	}
 
-}
+	var playerState 			=	PlayerState.MarioSmall;
 
-function Update ()
-{
+	var lives					: int			= 3;
+	var keys					: int			= 0;
+	var coins					: int			= 0;
+	var projectileFire			: GameObject;
+	var projectileSocketRight	: Transform;
+	var projectileSocketLeft	: Transform;
 
-}
+	var materialMarioStandard	: Material;
+	var materialMarioFire		: Material;
+
+	var changeMario				: boolean		= false;
+	var hasFire					: boolean		= false;
+
+	private var coinLife		: int			= 20;
+	private var canShoot		: boolean		= false;
+
+	function Update(){
+		var playerControls = GetComponent("playerControls");
+		if(changeMario){
+			SetPlayerState();
+		}
+		if(canShoot){
+			var clone;
+			var rb;
+			//fires a fire ball to the left when player is facing left
+			if(Input.GetButtonDown("Fire1") && projectileFire && playerControls.moveDirection == 0){
+				clone = Instantiate(projectileFire, projectileSocketLeft.transform.position, transform.rotation);
+				rb = clone.GetComponent("Rigidbody");
+				rb.AddForce(-90, 0, 0);
+			}
+			//fire ball to the right
+			if(Input.GetButtonDown("Fire1") && projectileFire && playerControls.moveDirection == 1){
+				clone = Instantiate(projectileFire, projectileSocketRight.transform.position, transform.rotation);
+				rb = clone.GetComponent("Rigidbody");
+				rb.AddForce(90, 0, 0);
+			}
+		}else{
+			return;
+		}
+
+	}
+
+	function AddKey(numKey : int){
+		keys += numKey;
+
+	}
+
+	function AddCoin(numCoin : int){
+		coins += numCoin;
+
+	}
+
+	function SetPlayerState(){
+		var playerControls = GetComponent("playerControls");
+		var characterController = GetComponent(CharacterController);
+
+		switch(playerState){
+			case PlayerState.MarioSmall	:
+				//print("mario small");
+				transform.GetComponent.<Renderer>().material = materialMarioStandard;
+				playerControls.gravity = 0.0;
+				transform.Translate(0, 0.2, 0);
+				transform.localScale = new Vector3(1.0, 0.75, 1.0);
+				characterController.height = 0.45;
+				playerControls.gravity = 20.0;
+				canShoot = false;
+				changeMario = false;
+				break;
+			case PlayerState.MarioLarge	:
+				//print("Mario large");
+				transform.GetComponent.<Renderer>().material = materialMarioStandard;
+				playerControls.gravity = 0.0;
+				transform.Translate(0, 0.2, 0);
+				transform.localScale = new Vector3(1.0, 1.0, 1.0);
+				characterController.height = 0.5;
+				playerControls.gravity = 20.0;
+				canShoot = false;
+				changeMario = false;
+				break;
+			case PlayerState.MarioFire	:
+				//print("mario fire");
+				transform.GetComponent.<Renderer>().material = materialMarioFire;
+				playerControls.gravity = 0.0;
+				transform.Translate(0, 0.2, 0);
+				transform.localScale = new Vector3(1.0, 1.0, 1.0);
+				characterController.height = 0.5;
+				playerControls.gravity = 20.0;
+				canShoot = true;
+				changeMario = false;
+				break;
+			case PlayerState.MarioDead	:
+				//print("mario dead");
+				transform.localScale = new Vector3(1.0, 1.0, 1.0);
+				Destroy(gameObject);
+				changeMario = false;
+				break;
+		}
+	}
+
+//finito
+
+
--- MarioBros2/Assets/2D Mario Assets/Scripts/spawnSaveSetup.js
+++ MarioBros2/Assets/2D Mario Assets/Scripts/spawnSaveSetup.js
@@ -1,11 +1,38 @@
-#pragma strict
+//Spawn Save player location setup
+//Descripton: Saves the player location (save points along the way for spawning after death)
 
-function Start ()
-{
+	var startPoint					: Transform;
+	var soundDie					: AudioClip;
+	private	var soundRate			: float = 0.0;
+	private var soundDelay			: float = 0.0;
+	private var curSavePosition		: Vector3;
 
-}
 
-function Update ()
-{
+	function playSoundFX(soundName, soundDelay){
+		if(!GetComponent.<AudioSource>().isPlaying && Time.time > soundRate){
+			soundRate = Time.time + soundDelay;
+			GetComponent.<AudioSource>().clip = soundName;
+			GetComponent.<AudioSource>().Play();
+			yield WaitForSeconds(GetComponent.<AudioSource>().clip.length);
+		}
+	}
 
-}
+	function OnTriggerEnter(other : Collider){
+		if(other.tag == "savePoint"){
+			curSavePosition = this.transform.position;
+			
+		}
+		if(other.tag == "killbox"){
+			playSoundFX(soundDie, 0);
+			transform.position = curSavePosition;
+
+		}
+	}
+
+	function Start (){
+		if(startPoint != null){
+			this.transform.position = startPoint.position;
+		}
+
+	}
+//finito
